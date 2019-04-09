5ff88328f3be56963c58e2e9c182f3a84bc69806
Manoel Carvalho <manoelmecat@gmail.com>
2019-04-09 16:45:39 -0300

    Get, All e Post

--- server.js
+++ server.js
@@ -1,25 +1,48 @@
 const express = require("express")
-const dados = require("./dados.json")
-const app = express()
+const sqlite  = require("sqlite")
+const bodyParser = require("body-parser")
+const app 	  = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+const parser = bodyParser.json()
+
+const dbCon = sqlite.open("banco.sqlite", {Promise})
+
+const init = async () => {
+	const db = await dbCon
+	await db.run("CREATE TABLE IF NOT EXISTS contatos(id INTEGER PRIMARY KEY, nome TEXT, idade INTEGER, email TEXT, url_image TEXT, endereco TEXT, contato TEXT);")
+	await db.run("INSERT INTO contatos(nome, idade, email, url_image, endereco, contato) VALUES ('JOsé', 23 ,'jose@gmail.com','user.jpg','Rua C','8899999999');")
+}
+
+//init()
 
 app.get('/', (req, res) => {
 	res.send("Início")
 })
 
-app.get('/pessoas', (req, res) => {
+app.get('/pessoas', async (req, res) => {
+	const db = await dbCon
+	const dados = await db.all("SELECT * FROM contatos")
 	res.send(JSON.stringify(dados))
 })
 
-app.get('/pessoas/:id', (req, res) => {
-	let id = req.params.id - 1
-	let dado = dados[id]
-
-	if(dados[id]){
-		res.send(JSON.stringify(dado))
+app.get('/pessoas/:id', async (req, res) => {
+	const db = await dbCon
+	let id = req.params.id
+	const dado = await db.get(`SELECT * FROM contatos WHERE id = ${id}`)
+	if(dado){
+		res.send(JSON.stringify(dado))		
 	}else{
-		res.send("O Id não corresponde a nenhum usuario")
+		res.send("Id não encontrado")
 	}
-	
+
+})
+
+app.post('/pessoas', parser, async (req, res) => {
+	let { nome, idade, email, url_image, endereco, contato} = req.body
+	const db = await dbCon
+	await db.get(`INSERT INTO contatos (nome, idade, email, url_image, endereco, contato) VALUES ('${nome}','${idade}','${email}','${url_image}','${endereco}','${contato}')`)
+	res.send(req.body)
 })
 
 
